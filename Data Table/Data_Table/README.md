f234c5b256fa6811941fbece7ffd5ac2a487b1a5
dhanu9726 <dhanashrigirase6@gmail.com>
2024-09-15 01:40:39 +0530

    DATA TABLE

---
+++ Data Table/Data_Table/README.md
@@ -0,0 +1,1 @@
+my video:"https://drive.google.com/file/d/1hplA6POekw_qeEa68FcmH8xHwRwujgy-/view?usp=sharing"
--- Data Table/Data_Table/src/App.jsx
+++ Data Table/Data_Table/src/App.jsx
@@ -2,15 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-import Allroutes from './routes/Allroutes'
+import Allroutes from './Allroutes'
 
 function App() {
-
   return (
     <>
-      <Navbar />
-      <Allroutes />
+     <Allroutes/>
     </>
   )
 }
