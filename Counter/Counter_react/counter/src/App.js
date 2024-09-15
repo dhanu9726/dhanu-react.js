df6843eccdbec242d85b081345c3981ec36c087a
dhanu9726 <dhanashrigirase6@gmail.com>
2024-09-15 20:36:44 +0530

    COUNTER

---
+++ Counter/Counter_react/counter/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState} from 'react';
+import Counter from './counter';
+
+function App() {
+ 
+  return (
+    <div className="App">
+     
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
